314eeffe32157390371ebc9c96aff73c2027e868
Andrey Luiz <andrey.luiz@scout24.ch>
2020-03-26 22:00:50 +0100

    Setup basic svelte app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-document.body.innerHTML = "Hello";
+import "core-js";
+import "regenerator-runtime/runtime";
+
+import App from "./App.svelte";
+
+new App({
+  target: document.getElementById("root")
+});
